6971f7a20843b22e02df5835c8663a26e4332ba5
MiguelRomeroGmx <contacto@miguelromero.mx>
2018-02-24 12:54:02 -0600

    Python

---
+++ holaMundo.py
@@ -0,0 +1,10 @@
+print ("hello world")
+base = input ("Introducir el precio del articulo: ")
+print (base)
+base = float(base)
+
+iva = (float(base) * 0.21)
+total = (float(base) + iva)
+print (total)
+print (base, total, iva)
+
